4183ea55ea910ab96a6fa73285643e56b5197a8d
Aglaya <agl.malcv@gmail.com>
2025-05-04 16:43:19 +0300

    Setup the game loop

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int main () {
+    Color darkBlue = {44, 44, 127, 255};
+
+    InitWindow(300, 600, "Tetris");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false){
+
+        BeginDrawing();
+        ClearBackground(darkBlue);
+
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+}
